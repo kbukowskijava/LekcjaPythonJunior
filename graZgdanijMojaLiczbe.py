56491eb6e89a58f56b999209986a7aab7240645f
Kacper Bukowski <kacper.bukowski@student.wat.edu.pl>
2022-06-02 20:00:53 +0200

    graZgadnijMojaLiczbe

---
+++ graZgdanijMojaLiczbe.py
@@ -0,0 +1,6 @@
+import random
+
+print("-------Zgadnij moją liczbę-------")
+print("Musisz znaleźc liczbę wybraną przez komputer! Liczba jest w przedziale od 1 do 10")
+magiczna_liczba = random.randint(1, 10)
+liczba_uzytkownika = 0
